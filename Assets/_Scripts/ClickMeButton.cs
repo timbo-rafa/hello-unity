ec323b157d8bbf7986f5111f0b641506a1b4cdab
Rafael Timbó <timbo.rafa@gmail.com>
2018-03-06 13:45:26 -0500

    Hello World Unity

---
+++ Assets/_Scripts/ClickMeButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickMeButton : MonoBehaviour {
+
+	private int times = 0;
+
+	public Button clickMeButton;
+
+	public Text helloLabel;
+	// Use this for initialization
+	void Start () {
+		this.clickMeButton.onClick.AddListener (this.clickHandler);
+	}
+
+	public void clickHandler() {
+		times = times + 1;
+		this.helloLabel.text = "Clicked " + times + " times!";
+	}
+}
